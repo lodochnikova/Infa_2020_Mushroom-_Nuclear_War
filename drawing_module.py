d86b1a2df1c958082700a1de97d110d86f159d6a
firsovviktor <72372722+firsovviktor@users.noreply.github.com>
2020-11-23 10:20:31 +0300

    Теперь и с отрисовкой бомб

--- drawing_module.py
+++ drawing_module.py
@@ -3,6 +3,11 @@
 # На выход выводится картинка
 # Для визуализации добавлены screen.fill(green)
 # и задан массив damage[x][y]=(x+y)/(w+h), w, h - размеры экрана
+
+# При вызове функции bomb(x, y) следует передать координаты бомбы
+# Вызывать взрыв бомбы для отрисовки отдельно не нужно
+# Однако массив damage здесь не обновляется
+# Для визуализации задан вызов бомб каждые 50 кадров
 import pygame
 import math
 import random
@@ -13,18 +18,42 @@ height = 600
 time = 0
 DARK_YELLOW = (128, 128, 0)
 BRIGHT_YELLOW = (255, 255, 0)
+boom = 40
 
 damage = []
 for i in range(width):
     damage.append([])
     for j in range(height):
         damage[i].append((i+j)/(width+height))
+bomb_time = []
+bomb_x = []
+bomb_y = []
+bomb_count = 0
+bomb_del = 0
 
 def draw():
-    global width, height, time
+    global width, height, time, bomb_del, bomb_x, bomb_y, bomb_time, bomb_count
+
+    print (time)
+
     for i in range(math.floor(width/10)):
         for j in range(math.floor(height/10)):
             rad_dot(10*i, 10*j)
+    for i in range(bomb_count):
+        draw_bomb(i)
+
+    bomb_del = 0
+    for i in range(bomb_count):
+        boom_check(i)
+    for i in range(bomb_del):
+        del bomb_x[0]
+        del bomb_y[0]
+        del bomb_time[0]
+        bomb_count -= 1
+
+    if (time % 30 == 0):
+        bomb(time % 800, time % 600)
+        bomb(time % 800 + 50, time % 600)
 
 def rad_dot(x, y):
     global damage, time
@@ -35,6 +64,36 @@ def rad_dot(x, y):
         b = 0
         Radius = 2 + math.sin(time2/30)
         circle (screen, (r, g, b), (x, y), Radius, 0)
+
+def bomb(x, y):
+    global bomb_time, bomb_x, bomb_y, bomb_count, time
+    bomb_x.append(x)
+    bomb_y.append(y)
+    bomb_time.append(time)
+    bomb_count += 1
+
+def bomb_color(t):
+    r = int(t/boom * 255)
+    c = (r, 0, 0)
+    return c
+
+def draw_bomb(i):
+    global bomb_time, bomb_x, bomb_y, bomb_count, boom
+    dt = time - bomb_time[i]
+    x = bomb_x[i]
+    y = bomb_y[i]
+    color = bomb_color(dt)
+    radius = 8
+    blast_radius = 20
+    circle (screen, color, (x, y), radius, 0)
+    if (dt >= boom):
+        circle (screen, (255, 255, 0), (x, y), blast_radius, 0)
+
+def boom_check(i):
+    global bomb_time, time, boom, bomb_del
+    if (time - bomb_time[i] >= boom):
+        bomb_del += 1
+
 
 pygame.init()
 FPS = 30
